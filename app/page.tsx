b8c1ed8c7a25a52d81ee2905596329936b0f86a8
kncseller <kncseller@gmail.com>
2025-10-09 19:22:33 +0700

    aaaaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -2,47 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
     <div className="">
-        
+      
     </div>
-    <div className="screen_box">
-
-  <div className="box-row-col">
-
-      <div className="a-col">
-
-      </div>
-
-      <div className="a-col a-auto">
-
-        <div  className='imgbox'>
-
-          
-
-        </div>
-
-        <div className="centerloading">
-
-          <div className="lds-ripple"><div></div><div></div></div>
-
-        </div>
-
-      </div>
-
-      <div className="a-col">
-
-      </div>
-
-  </div>
-
-</div>
-
-  <div className="loadingapp">
-
-  <div className="loader">Loading...</div>
-
-</div>
-    </>
   );
 }
